89d85248ba7b4648d8ead380eb4de42f3091b9b4
KenzieAlyce <mackenziealyce@gmail.com>
2021-03-13 09:50:58 -0600

    Add files via upload

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+var burger = {
+    all:function(cb){
+        orm.selectAll("burgers", res => {
+            cb(res)
+        })
+    },
+    create:function(cols, vals, cb){
+         orm.insertOne("burgers", cols, vals, function(res){
+             cb(res)
+         })
+    },
+    update:function(fieldsKeyVal, whereKeyVal, cb){
+        orm.updateOne("burgers", fieldsKeyVal, whereKeyVal, function(res){
+            cb(res)
+        })
+    }
+    //create:
+};
+
+
+module.exports = burger;
