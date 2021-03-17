c077767991b75c6bc921e4921f2741c2acda6745
KenzieAlyce <mackenziealyce@gmail.com>
2021-03-16 22:45:11 -0500

    fixed put method

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,45 +1,53 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 var router = express.Router();
 
 var burger = require(path.resolve(__dirname, "../models/burger.js"));
 
 router.get("/", (req, res) => {
-  burger.all(data => {
-      var handleBarsObj = {
-          burgers: data 
-      }
-      res.render("index", handleBarsObj)
+  burger.all((data) => {
+    var handleBarsObj = {
+      burgers: data,
+    };
+    res.render("index", handleBarsObj);
   });
 });
 
 router.post("/burgers", (req, res) => {
-  console.log(">",req.body.burger_name)
- burger.create(['burger_name'], [req.body.burger_name], data => {
-  res.redirect("/");
-});
+  console.log(">", req.body.burger_name);
+  burger.create(["burger_name"], [req.body.burger_name], (data) => {
+    res.redirect("/");
+  });
 });
 
 router.put("/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
-console.log('condition: ', condition)
-  burger.update({
-      devoured: true
-  }, condition, function (data) {
+  console.log("condition: ", condition);
+  burger.update(
+    {
+      devoured: true,
+    },
+    condition,
+    function (data) {
       res.redirect("/");
-
-  });
+    }
+  );
 });
 
-// router.put("burgers/:id", (req, res) => {
-//  burger.update({devoured: (req.body.devoured) ? true:false},
-//  {
-//      id: req.params.id
-//  }, data => {
-//   if (data.changedRows === 0) {
-//       return res.status(404).end()
-//   }
-//   res.status(200).end()
-//  });
+// router.put("/burgers/:id", (req, res) => {
+//   console.log("req.body.devoured: ", req.body.devoured);
+//   burger.update(
+//     { devoured: req.body.devoured ? true : false },
+//     {
+//       id: req.params.id,
+//     },
+//     (data) => {
+//       if (data.changedRows === 0) {
+//         return res.status(404).end();
+//       }
+//       res.status(200).end();
+//     }
+//   );
 // });
+
 module.exports = router;
